9c71723b38cf1613a2e6204904f2fcb35846f98e
rockgold0911 <rockgold0911@gmail.com>
2022-09-13 17:19:52 +0800

    feat(example): react update

--- apps/react-example/src/main.tsx
+++ apps/react-example/src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { ConfigProvider } from '@arco-design/web-react'
 import enUS from '@arco-design/web-react/es/locale/en-US'
 import { NFT3Provider } from '@nft3sdk/did-manager'
+import '@nft3sdk/did-manager/dist/cjs/components/style.css'
 import '@arco-design/web-react/dist/css/arco.css'
 
 import './assets/global.scss'
@@ -11,7 +12,7 @@ const endpoint = 'http://t0.onebitdev.com:10000/'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ConfigProvider locale={enUS}>
-    <NFT3Provider endpoint={endpoint} silent>
+    <NFT3Provider endpoint={endpoint}>
       <App />
     </NFT3Provider>
   </ConfigProvider>
